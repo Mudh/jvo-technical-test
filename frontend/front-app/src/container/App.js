4d2d84d2e9c12ba3ed5bc1bb3e586e27954cb222
John Berry <jonmudh@gmail.com>
2019-11-08 18:53:42 +0100

    Add App container + store provider - Level 4

---
+++ frontend/front-app/src/container/App.js
@@ -0,0 +1,25 @@
+import { connect } from "react-redux";
+
+import { incrementCurrent, randomIncrement } from "../store/actions";
+import { fetchObjectives } from "../store/operations";
+import { fromObjectives } from "../store/selectors";
+import App from "../App";
+
+const mapStateToProps = state => ({
+  data: fromObjectives.data(state),
+  currentDate: fromObjectives.currentDate(state),
+  currentOvertarget: fromObjectives.currentOvertarget(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchObjectives: () => dispatch(fetchObjectives()),
+  incrementCurrent: index => dispatch(incrementCurrent(index)),
+  randomIncrement: () => dispatch(randomIncrement())
+});
+
+const AppContainer = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+
+export default AppContainer;
