18ee1b07215ced02a56093d7e5f1438d7d95b0df
John Berry <jonmudh@gmail.com>
2019-11-08 22:37:24 +0100

    Create recursive selector to add nested parent/children objectives - Level 4

--- frontend/front-app/src/store/objectives/selectors.js
+++ frontend/front-app/src/store/objectives/selectors.js
@@ -7,3 +7,30 @@ export const currentOvertarget = createSelector(
   data,
   objectives => objectives.filter(el => el.current > el.target).length
 );
+
+export const nestedData = createSelector(
+  data,
+  objectives => {
+    const getNestedChildren = (arr, parent) => {
+      let out = [];
+      for (let i in arr) {
+        if (arr[i].parent_id === parent) {
+          const children = getNestedChildren(arr, arr[i].id);
+
+          if (children.length) {
+            arr[i].children = children;
+          }
+          out.push(arr[i]);
+        }
+      }
+      return out;
+    };
+
+    const nestedObjectives = (arr, parent) => {
+      const mainParent = arr.filter(el => !el.parent_id);
+      const children = getNestedChildren(arr, parent);
+      return [{ ...mainParent, children: [{ ...children }] }];
+    };
+    return nestedObjectives(objectives, 1);
+  }
+);
