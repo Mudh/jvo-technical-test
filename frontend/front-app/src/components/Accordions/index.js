a040403a987df5a8f5595482fa415a25a4ddd081
John Berry <jonmudh@gmail.com>
2019-11-09 10:54:20 +0100

    Accordion component style + data display - Level 4

--- frontend/front-app/src/components/Accordions/index.js
+++ frontend/front-app/src/components/Accordions/index.js
@@ -1,15 +1,50 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { LineChart } from "react-chartkick";
 import Collapsible from "react-collapsible";
 
-const Item = ({ data }) => {
+const AccordionTree = ({ data }) => {
+  const level = 1;
+  const haschildren = item => {
+    return item.children && item.children.length;
+  };
+
+  const AccordionTreeHeader = ({ title }) => {
+    return (
+      <>
+        <span className="App-accordion-triangle"></span>
+        <span className="App-accordion-decoration"></span>
+        <span className="App-accordion-title">{title}</span>
+      </>
+    );
+  };
+
   return (
-    <div>
-      {data.map(m => {
+    <div className="App-accordion">
+      {data.map((item, i) => {
         return (
-          <Collapsible>
-            <p>{m.title}</p>
-            {m.children && <Item data={m.children} key={m.id} />}
+          <Collapsible
+            classParentString="App-accordion-item"
+            trigger={
+              <AccordionTreeHeader title={item.title}></AccordionTreeHeader>
+            }
+            key={level + i}
+          >
+            <LineChart
+              id={`chart_${item.id}`}
+              width="71vw"
+              height={180}
+              curve={true}
+              colors={["#ff6347"]}
+              data={{
+                [item.start_date]: item.start,
+                [item.currentDate]: item.current,
+                [item.end_date]: item.target
+              }}
+            />
+            {haschildren(item) && (
+              <AccordionTree data={item.children} level={level + 1} />
+            )}
           </Collapsible>
         );
       })}
@@ -18,9 +53,9 @@ const Item = ({ data }) => {
 };
 
 const Accordion = ({ objectives }) => {
-  console.log("nested data", objectives);
+  console.log("nested data", objectives.map(item => item));
 
-  return <Collapsible trigger="Start here"></Collapsible>;
+  return <AccordionTree data={objectives}></AccordionTree>;
 };
 
 Accordion.propTypes = {
